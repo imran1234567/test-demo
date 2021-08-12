9e8aee821a52db216b8349ac4f46ffd3e138f0f2
Sk Imran <sk2read@gmail.com>
2021-08-12 08:48:30 +0530

    Disconnect from the current value

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 // data binding
 let ten = 10;
-console.log(ten*ten);
+console.log(ten * ten);
+
+// the = opearator can disconect from the exsting
+// binding from the current value
+let mood = "light";
+console.log(mood);
+
+mood = "Dark";
+console.log(mood);
